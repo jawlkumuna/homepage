abde3bf5195ecef06f00b2454ba8cb0233638383
András Seres <91373059+jawlkumuna@users.noreply.github.com>
2024-07-16 14:29:01 +0200

    add uploads

---
+++ app/gallery/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between px-80 py-24">
+      <div className="flex w-full flex-row justify-between">
+        <h1 className="text-left text-5xl font-bold">
+          Welcome to the{" "}
+          <span className="text-purple-400 underline">gallery</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
