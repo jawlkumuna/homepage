70282d0c9bc84e24c02dc0039b200836421b5a28
András Seres <91373059+jawlkumuna@users.noreply.github.com>
2024-07-16 14:42:02 +0200

    fix ts error

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename');
+  const filename = searchParams.get('filename') ?? "";
 
   // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
+  const blob = await put(filename, request.body as any, {
     access: 'public',
   });
 
